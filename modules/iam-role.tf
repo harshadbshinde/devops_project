37b1f4646ffac43f545e7dbca4ccf29b979bc871
Harshad Shinde <harshadbshinde8754@gmail.com>
2026-08-18 14:24:18 +0530

    Update iam-role.tf

--- modules/iam-role.tf
+++ modules/iam-role.tf
@@ -1,48 +1,70 @@
-data "aws_iam_role" "eks_cluster_role" {
+resource "aws_iam_role" "eks_cluster_role" {
 
   name = "eks-cluster-role"
 
-}
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
 
+    Statement = [{
+      Effect = "Allow"
 
-data "aws_iam_role" "node_role" {
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
 
-  name = "eks-node-role"
-
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
 
-  role = data.aws_iam_role.eks_cluster_role.name
+  role = aws_iam_role.eks_cluster_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
 
+
+resource "aws_iam_role" "node_role" {
+
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 
 resource "aws_iam_role_policy_attachment" "worker1" {
 
-  role = data.aws_iam_role.node_role.name
+  role = aws_iam_role.node_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-
 }
 
 
 resource "aws_iam_role_policy_attachment" "worker2" {
 
-  role = data.aws_iam_role.node_role.name
+  role = aws_iam_role.node_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-
 }
 
 
 resource "aws_iam_role_policy_attachment" "worker3" {
 
-  role = data.aws_iam_role.node_role.name
+  role = aws_iam_role.node_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-
 }
